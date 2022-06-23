a1a1001baecea212637ca1e67a7fdcae8661257d
Steven Wyatt <sw.coedwr@gmail.com>
2022-06-23 14:33:31 +0100

    add minimal theme to plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -17,7 +17,8 @@ df %>%
   ggplot(aes(x = Temp9am, y = Humidity9am),
          ) + 
   geom_point(colour = 'red') +
-  labs(title = 'Temperature vs Humidity')
+  labs(title = 'Temperature vs Humidity') +
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
