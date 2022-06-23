4c50b3a400ab4a8979bdf7f2fe24ba7dcfd542c5
Steven Wyatt <sw.coedwr@gmail.com>
2022-06-23 15:51:18 +0100

    add rainfall time series chart

--- R/create-plot.R
+++ R/create-plot.R
@@ -21,5 +21,13 @@ df %>%
   theme_minimal() + 
   facet_wrap(~WindDir9am)
 
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = time_series)
+
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
